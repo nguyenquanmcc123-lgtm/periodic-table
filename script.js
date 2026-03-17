640e7c36c2996310bd8d1a7ba62d9527a1cd5ce2
nguyenquanmcc123-lgtm <nguyenquanmcc123@gmail.com>
2026-03-17 23:25:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
-const table = document.getElementById("table");
-const info = document.getElementById("info");
+body{
+margin:0;
+font-family:Arial;
+background:#020617;
+color:white;
+}
 
-const elements = [
-{symbol:"H",name:"Hydrogen",number:1},
-{symbol:"He",name:"Helium",number:2},
-{symbol:"Li",name:"Lithium",number:3},
-{symbol:"Be",name:"Beryllium",number:4},
-{symbol:"B",name:"Boron",number:5},
-{symbol:"C",name:"Carbon",number:6},
-{symbol:"N",name:"Nitrogen",number:7},
-{symbol:"O",name:"Oxygen",number:8},
-{symbol:"F",name:"Fluorine",number:9},
-{symbol:"Ne",name:"Neon",number:10},
-{symbol:"Na",name:"Sodium",number:11},
-{symbol:"Mg",name:"Magnesium",number:12},
-{symbol:"Al",name:"Aluminium",number:13},
-{symbol:"Si",name:"Silicon",number:14},
-{symbol:"P",name:"Phosphorus",number:15},
-{symbol:"S",name:"Sulfur",number:16},
-{symbol:"Cl",name:"Chlorine",number:17},
-{symbol:"Ar",name:"Argon",number:18}
-];
+header{
+text-align:center;
+padding:15px;
+background:#0f172a;
+}
 
-function createTable(){
+#layout{
+display:grid;
+grid-template-columns:300px 1fr;
+height:100vh;
+}
 
-elements.forEach(el=>{
+#sidebar{
+background:#0f172a;
+padding:15px;
+}
 
-const div=document.createElement("div");
+button{
 
-div.className="element";
-
-div.innerHTML=`
-<span>${el.number}</span>
-<b>${el.symbol}</b>
-`;
-
-div.onclick=()=>showInfo(el);
-
-table.appendChild(div);
-
-});
+background:#3b82f6;
+border:none;
+color:white;
+padding:8px 12px;
+margin:4px;
+border-radius:6px;
+cursor:pointer;
 
 }
 
-function showInfo(el){
-
-info.innerHTML=`
-<h2>${el.name}</h2>
-<p>Symbol: ${el.symbol}</p>
-<p>Atomic Number: ${el.number}</p>
-`;
-
+button:hover{
+background:#2563eb;
 }
 
-createTable();
+#viewer{
+width:100%;
+height:100%;
+}
+
+#three{
+width:100%;
+height:100%;
+}
